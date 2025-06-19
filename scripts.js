440b13a43cf568b947b7d3de8623a590d37272da
splashes55 <32332741+splashes55@users.noreply.github.com>
2025-06-19 16:40:26 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 // 🟨 Ortak Tanımlar
-const NOCODE_URL = "https://script.google.com/macros/s/AKfycbyUTY7OIKvGoRzCVLU2zQMvwvQlTnbSo5VvyYvBb02w3NxhbQzC3FW7T7vi4j9a7JQszA/exec"; // kendi Apps Script URL'in
+const NOCODE_URL = "https://script.google.com/macros/s/AKfycbx_txJCZMXL1jEW8VfAcE8U4IzcGypQlvTWhYPXlWOVnBIS_wtz0rDotWRRHMqchP5sFA/exec"; // kendi Apps Script URL'in
 const SHEET_MACLAR = "Maclar";
 const SHEET_OYUNCULAR = "Oyuncular";
 const SHEET_OYLAR = "Oylar";
